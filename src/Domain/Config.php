a891f8059652d313949ece7fc8479442e3e94576
Andrea <carminati.andra96@gmail.com>
2026-01-19 08:38:55 +0100

    aggiunta classe config

---
+++ src/Domain/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+class Config {
+    private $currency;
+    private $logTransactions;
+    private $minimoDeposito;
+    private $massimoDeposito;
+    private $limitePrelievoGiornaliero;
+    private $minimoPrelievo;
+    private $massimoPrelievo;
+}
